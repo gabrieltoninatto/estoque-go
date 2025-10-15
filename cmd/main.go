79e20b4347f58e9b959686ec1b9399451d816435
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2025-10-15 16:47:57 -0300

    Refatora o main.go para utilizar o serviço de estoque e adiciona a função AddItem no pacote services

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"estoque/internal/models"
+	"estoque/internal/services"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Sistema de Estoque")
-
-	item1 := models.Item{
-		ID:       1,
-		Name:     "Camiseta",
-		Quantity: 10,
-		Price:    29.99,
-	}
-
-	item2 := models.Item{
-		ID:       2,
-		Name:     "Mochila",
-		Quantity: 5,
-		Price:    49.99,
-	}
-
-	fmt.Println(item1.Info())
-	fmt.Println(item2.Info())
+	estoque := services.NewEstoque()
+	//estoque.AddItem()
+	fmt.Println(estoque)
 
 }
--- internal/services/estoque.go
+++ internal/services/estoque.go
@@ -1,6 +1,10 @@
 package services
 
-import "estoque/internal/models"
+import (
+	"estoque/internal/models"
+	"fmt"
+	"strconv"
+)
 
 type Estoque struct {
 	items map[string]models.Item
@@ -12,4 +16,16 @@ func NewEstoque() *Estoque {
 		items: make(map[string]models.Item),
 		logs:  []models.Log{},
 	}
+}
+
+func (e *Estoque) AddItem(item models.Item) error {
+	if item.Quantity <= 0 {
+		return fmt.Errorf("erro ao adicionar item: [ID: %s] possui uma quantidade (zero ou negativa)", item.ID)
+	}
+	existingItem, exists := e.items[strconv.Itoa(item.ID)]
+	if exists {
+		item.Quantity += existingItem.Quantity
+	}
+	e.items[strconv.Itoa(item.ID)] = item
+	return nil
 }
