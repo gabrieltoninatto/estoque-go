bd835dd9d589005687c155ab3e4621af642d1c09
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2025-10-16 16:30:22 -0300

    Adiciona itens ao sistema de estoque na função main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"estoque/internal/models"
 	"estoque/internal/services"
 	"fmt"
 )
@@ -8,7 +9,19 @@ import (
 func main() {
 	fmt.Println("Sistema de Estoque")
 	estoque := services.NewEstoque()
-	//estoque.AddItem()
+	itens := []models.Item{
+		{ID: 1, Name: "Fone", Quantity: 10, Price: 100},
+		{ID: 1, Name: "Camiseta", Quantity: 1, Price: 55.99},
+		{ID: 1, Name: "Mouse", Quantity: 2, Price: 12.99},
+	}
+
+	for _, item := range itens {
+		err := estoque.AddItem(item)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
 	fmt.Println(estoque)
 
 }
